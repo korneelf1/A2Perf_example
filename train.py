ccd633820eb536efc4a70b356adf5936909ae405
korneelf1 <korneel.vandenberghe@hotmail.be>
2023-12-06 12:51:26 -0500

    Initial notebook

--- train.py
+++ train.py
@@ -1,15 +1,20 @@
+# 1) Import the necessary packages. For this tutorial, we will use the `quadruped_locomotion` environment. 
+
+# import the relevant A2Perf domain
 import rl_perf
 import rl_perf.domains.quadruped_locomotion
 
+# import gymnasium to create the environment
 import gymnasium as gym
 
+# import the abseil app to run the experiment
+from absl import app
+
+# import packages needed for your training
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-from absl import app
-
-
-
+# 2) Next, we define our training function. This function will be called by the abseil app.
 def train():
     '''Include your training algorithm here.'''
     # Create the environment
@@ -24,10 +29,12 @@ def train():
 
     del model # remove to demonstrate saving and loading
 
+# 3) Optionally, we define the main function. This function will be called when the script is run directly.
 def main(_):
   # The main function where the training process is initiated.
   train()
 
 
 if __name__ == '__main__':
+  # Run the main function using the abseil app. This allows us to pass command line arguments to the script.
   app.run(main)
